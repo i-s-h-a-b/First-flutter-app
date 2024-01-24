59399d943d0ec043e473d8e2343c7079a0df8f16
i-s-h-a-b <113613452+i-s-h-a-b@users.noreply.github.com>
2024-01-24 09:23:38 +0530

    Update1 main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 0, 255, 242),
-              Color.fromARGB(255, 0, 132, 255),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(color: Colors.red, fontSize: 28),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
