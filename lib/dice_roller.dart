d0ae4656f7d56d5fb18faba279a3b725f7fa3de4
i-s-h-a-b <113613452+i-s-h-a-b@users.noreply.github.com>
2024-01-25 08:34:31 +0530

    Add files via upload

---
+++ lib/dice_roller.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/images/dice-2.png';
+
+  void rollDice() {
+    activeDiceImage = 'assets/images/dice-4.png';
+    print('Changing image'); //for debug console
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
+        ),
+        const SizedBox(
+            height: 20), // alternative -- padding:const EdgeInsets.all(20),
+        TextButton(
+          onPressed:
+              rollDice, //passing a pointer to hte previously defined function. not executing it here just pointing to it.
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 30),
+          ),
+          child: const Text('Roll Dice!'),
+        )
+      ],
+    );
+  }
+}
